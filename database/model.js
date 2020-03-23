f592c6195c0c156903d4ea48a28983a0d8719ac9
Sam Hodak <slhodak@gmail.com>
2020-03-23 00:53:28 -0700

    Move feed retrieval to model

--- database/model.js
+++ database/model.js
@@ -1,6 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const sampleFeed = path.resolve('./data/sampleFeed.json');
+
 const model = {
   saveMessage: (message) => {
     console.log(`Saving message: ${JSON.stringify(message)}`);
+  },
+  getFeed: (terms, res) => {
+    fs.readFile(sampleFeed, (err, data) => {
+      if (!err) {
+        res.send(data);
+      } else {
+        const errorResponse = {
+          message: 'Sorry, there was an error fetching the resource at /feed'
+        };
+        res.send(500, JSON.stringify(errorResponse));
+      }
+    });
   }
 }
 
--- server/util.js
+++ server/util.js
@@ -1,20 +1,4 @@
-const path = require('path');
-
-const sampleFeed = path.resolve('./data/sampleFeed.json');
-
 const utils = {
-  getFeed: () => {
-    fs.readFile(sampleFeed, (err, data) => {
-      if (!err) {
-        res.send(data);
-      } else {
-        const errorResponse = {
-          message: 'Sorry, there was an error fetching the resource at /feed'
-        };
-        res.send(500, JSON.stringify(errorResponse));
-      }
-    });
-  }
 };
 
 module.exports = utils;
