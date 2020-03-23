87e3347f6fa6347cfade3e3d5da15b0658fd21de
Sam Hodak <slhodak@gmail.com>
2020-03-23 01:08:37 -0700

    Remove unused require

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const serverUtils = require('./util');
 const model = require('../database/model');
 
 app.use(express.json());
