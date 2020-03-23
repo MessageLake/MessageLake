54b38fc99e38dfa33230b9f9d0874e6f45ce89db
Sam Hodak <slhodak@gmail.com>
2020-03-22 19:09:50 -0700

    Set-up routes to post and get messages/feeds

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use('/', (req, _res, next) => {
+  console.debug(`url=${req.url}`);
+  next();
+});
+
+app.use('/home', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(301, '/home');
+});
+
+app.post('/message', (req, res) => {
+  console.log('Recieved a message');
+});
+
+app.get('/message', (req, res) => {
+  res.redirect(301, '/home');
+});
+
+app.get('/feed', (req, res) => {
+  res.redirect(301, '/home');
+});
+
+app.listen(PORT, () => {
+  `MessageLake listening on port ${PORT}`;
+});
