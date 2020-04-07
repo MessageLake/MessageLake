0d8068063bb49f4431ed7b7449452aeb0820267b
Sam Hodak <slhodak@gmail.com>
2020-04-07 16:02:30 -0700

    Init basic server for auth proj

--- auth/server.js
+++ auth/server.js
@@ -1,1 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  `Auth server listening at ${process.env.PORT}`;
+});
