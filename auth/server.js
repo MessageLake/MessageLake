deecb8d81b1b1faa8a5c1d48619a35e12997cfaa
Sam Hodak <slhodak@gmail.com>
2020-04-07 16:17:00 -0700

    Add oidc with okta config

--- auth/server.js
+++ auth/server.js
@@ -1,8 +1,33 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  `Auth server listening at ${process.env.PORT}`;
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: false
+}));
+
+const oidc = new ExpressOIDC({
+  issuer: `${process.env.OKTA_DOMAIN}/oauth2/default`,
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  redirect_uri: process.env.REDIRECT_URI,
+  scope: 'openid profile'
 });
+
+app.use(oidc.router);
+
+oidc.on('ready', () => {
+  app.listen(process.env.PORT, () => {
+    `Auth server listening at ${process.env.PORT}`;
+  });
+});
+
+oidc.on('error', (err) => {
+  console.error('Unable to configure ExpressOIDC', err);
+});
+
